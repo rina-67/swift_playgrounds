247da6d8df5042748ba2c096b17eccab1ad0b687
Rina Suzuki <58870050+rina-67@users.noreply.github.com>
2020-06-25 20:25:18 +0900

    こめ

--- swift/demo2/index.swift
+++ swift/demo2/index.swift
@@ -3,11 +3,13 @@ import PlaygroundSupport
 struct FirstSample: View {
     @State private var flag: Bool = true
     var body: some View { 
+        //alignment: .leading で左端に揃えられる
         VStack (alignment: .leading){
             Text("Toggle✨")
                 .padding()
                 .font(.largeTitle)
                 .background(Color.blue)
+            //toggleの実装
             Toggle(isOn: $flag, label: {
                 Text("ClickMe!")
             })
